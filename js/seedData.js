7af4fdf5b26d8f70c7d0c395cf166566d63b95e2
Joshua S <Joshua_Saun@protonmail.com>
2024-07-16 11:43:07 -0500

    Style: Added old stylesheets

---
+++ js/seedData.js
@@ -0,0 +1,22 @@
+const navigation_items = [
+  {
+    name: "Home",
+    icon: "fa-house",
+    link: "/index.html",
+  },
+  {
+    name: "Projects",
+    icon: "fa-folder",
+    link: "/projects.html",
+  },
+  {
+    name: "About",
+    icon: "fa-solid fa-address-card",
+    link: "/about.html",
+  },
+  {
+    name: "Register",
+    icon: "fa-solid fa-right-to-bracket",
+    link: "/register.html",
+  },
+];
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,55 @@
 // export function greet(name) {
 //   return `Hello, ${name}`;
 // }
+
+export function generateNavbar(items) {
+  const navUl = document.getElementById("nav-ul");
+  items.forEach((item) => {
+    const a = document.createElement("a");
+    const li = document.createElement("li");
+    a.href = item.link;
+    a.innerHTML = `<i class="fa ${item.icon}"></i>`;
+    a.appendChild(document.createTextNode(item.name));
+    a.appendChild(li);
+    navUl.appendChild(a);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  generateNavbar(navigation_items);
+});
+
+generateNavbar();
+
+export function toggleDarkMode() {
+  const lightModeBtn = document.getElementById("lightMode");
+  const darkModeBtn = document.getElementById("darkMode");
+  const body = document.body;
+
+  // Initial setup: check if dark mode is enabled
+  const isDarkMode = localStorage.getItem("darkMode") === "enabled";
+
+  if (isDarkMode) {
+    body.classList.add("darkMode");
+    darkModeBtn.classList.add("hidden");
+    lightModeBtn.classList.remove("hidden");
+  }
+
+  lightModeBtn.addEventListener("click", () => {
+    body.classList.remove("darkMode");
+    darkModeBtn.classList.remove("hidden");
+    lightModeBtn.classList.add("hidden");
+    localStorage.setItem("darkMode", "disabled");
+  });
+
+  darkModeBtn.addEventListener("click", () => {
+    body.classList.add("darkMode");
+    darkModeBtn.classList.toggle("hidden");
+    lightModeBtn.classList.toggle("hidden");
+    localStorage.setItem("darkMode", "enabled");
+  });
+  // console.log(localStorage);
+}
+
+toggleDarkMode();
+// localStorage.clear();
